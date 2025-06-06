80fc77653c5829c7c755812d0785bccac60c38d2
Jackmeson1 <130693642+Jackmeson1@users.noreply.github.com>
2025-06-05 23:03:03 -0700

    Update setup configuration

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="fnbi-automated-testing",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "pytest",
         "selenium",
@@ -21,7 +25,7 @@ setup(
     author="Your Name",
     author_email="your.email@example.com",
     description="Automated testing suite for FortiNBI application",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/fnbi-automated-testing",
     classifiers=[
